0b387f82980c4430176834a0890eae3e31b261aa
Ján Kostor <ko.janci@gmail.com>
2026-01-24 07:52:59 +0100

    swagger support

--- KIFRIOSSE.ASTFRI.Web.API/Program.cs
+++ KIFRIOSSE.ASTFRI.Web.API/Program.cs
@@ -13,12 +13,18 @@ namespace KIFRIOSSE.ASTFRI.Web.API
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
 
+            // swagger support
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
 
             app.UseHttpsRedirection();
