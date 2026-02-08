42114f59216af287cdd553b6b18769ac94c8d840
Ján Kostor <ko.janci@gmail.com>
2026-02-08 10:05:48 +0100

    lib project init

---
+++ KIFRIOSSE.ASTFRI.SDK/AstfriCLI.cs
@@ -0,0 +1,66 @@
+﻿using System.Runtime.InteropServices;
+
+namespace KIFRIOSSE.ASTFRI.SDK
+{
+    public class AstfriCLI
+    {
+        public Configuration Config { get; init; }
+
+        public AstfriCLI(Configuration config)
+        {
+            Config = config;
+        }
+
+        /// <summary>
+        /// spusti ASTFRI CLI transformacu dat
+        /// </summary>
+        /// <param name="inputLib">typ vstupnej kniznice</param>
+        /// <param name="inputText">zdrojovy kod na vstupe</param>
+        /// <param name="outputLib">typ vystupnej kniznice</param>
+        /// <returns>
+        ///     vystup transformacie ako string
+        /// </returns>
+        public string RunTranslation(string inputLib, string inputText, string outputLib)
+        {
+            throw new NotImplementedException("This method is not implemented yet. It should run the ASTFRI CLI with the given input and output libraries and return the result as a string.");
+        }
+
+        /// <summary>
+        /// zisti verziu ASTFRI CLI, aby sme mohli overit kompatibilitu a informovat pouzivatela o podporovanych funkciach
+        /// </summary>
+        /// <returns></returns>
+        public string GetVersion()
+        {
+            var process = new System.Diagnostics.Process
+            {
+                StartInfo = new System.Diagnostics.ProcessStartInfo
+                {
+                    FileName = Config.ExecutablePath,
+                    Arguments = "--version",
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                }
+            };
+
+            string astfriOutput = string.Empty;
+            using (var process = System.Diagnostics.Process.Start(processInfo))
+            {
+                if (process is not null)
+                {
+                    astfriOutput = process.StandardOutput.ReadToEnd();
+                    process.WaitForExit();
+                }
+            }
+
+            return astfriOutput.Trim();
+        }
+
+        public class Configuration
+        {
+            public string ExecutablePath { get; init; } = "astfri";
+            public string[] InputLibs { get; init; } = Array.Empty<string>();
+            public string[] OutputLibs { get; init; } = Array.Empty<string>();
+        }
+    }
+}
